ebad1558a925333f51e89a7085fbb76c943b6823
ossowskid <77751878+ossowskid@users.noreply.github.com>
2025-03-05 12:51:27 +0100

    feat: added component MoviesList with lazy, and routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { CircularProgress } from '@mui/material';
+
+const MoviesList = lazy(() => import('./components/MoviesList/MoviesList'));
+
 function App() {
-  return <>"Hello World"</>;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<CircularProgress />}>
+        <Routes>
+          <Route path='/' element={<MoviesList />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
 export default App;
