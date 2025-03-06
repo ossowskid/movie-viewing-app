9a755c080e7e7ba4ce9dc03fc44736667456f5bf
ossowskid <77751878+ossowskid@users.noreply.github.com>
2025-03-07 00:22:32 +0100

    feat: created  component AdditionalInformations which handle vote average, vote count and homepage

---
+++ src/components/movieDetails/additionalInformations/AdditionalInformations.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const RatingWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  gap: 4px;
+`;
---
+++ src/components/movieDetails/additionalInformations/AdditionalInformations.types.ts
@@ -0,0 +1,5 @@
+export type AdditionalInformationsProps = {
+  voteAverage: number;
+  voteCount: number;
+  homepage: string;
+};
