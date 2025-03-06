2af2eb70598cd4e2c5934bd6b11c03240598cfb4
ossowskid <77751878+ossowskid@users.noreply.github.com>
2025-03-07 00:41:16 +0100

    feat: added MovieDetails to AppRoute and created routes.ts which handle AppRoute's as constant as easier way to pick which url wants to use. After that changed paths in App.tsx to use AppRoute routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import { CircularProgress } from '@mui/material';
+import { AppRoute } from './routes/routes';
 
 const MoviesList = lazy(() => import('./components/MoviesList/MoviesList'));
+const MovieDetails = lazy(
+  () => import('./components/movieDetails/MovieDetails')
+);
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<CircularProgress />}>
         <Routes>
-          <Route path='/' element={<MoviesList />} />
+          <Route path={AppRoute.Home} element={<MoviesList />} />
+          <Route path={AppRoute.MovieDetails} element={<MovieDetails />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
