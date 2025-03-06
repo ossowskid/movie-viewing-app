287fab449342fec8d0c600c45bef73dd57951992
ossowskid <77751878+ossowskid@users.noreply.github.com>
2025-03-07 00:39:14 +0100

    refactor: changed useFormatDate to have another prop customDateFormat, that allow user to choose which date format wants to show

--- src/hooks/useFormatDate.ts
+++ src/hooks/useFormatDate.ts
@@ -2,11 +2,18 @@ import { useMemo } from 'react';
 import { format } from 'date-fns';
 import { pl } from 'date-fns/locale';
 
-const useFormattedDate = (dateString: string | null) => {
+const useFormattedDate = (
+  dateString: string | null,
+  customDateFormat?: string
+) => {
   return useMemo(() => {
     if (!dateString) return 'Brak daty';
     try {
-      return format(new Date(dateString), 'd MMM yyyy', { locale: pl });
+      return format(
+        new Date(dateString),
+        customDateFormat ? customDateFormat : 'd MMM yyyy',
+        { locale: pl }
+      );
     } catch {
       return 'Nieprawidłowa data';
     }
