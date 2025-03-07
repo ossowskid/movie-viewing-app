ecee3b44ceaa1bccee28926a0e59ed41324f6e41
ossowskid <77751878+ossowskid@users.noreply.github.com>
2025-03-07 10:39:19 +0100

    fix: after CR changed file name from routes.ts to AppRoute.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import { CircularProgress } from '@mui/material';
-import { AppRoute } from './routes/routes';
+import { AppRoute } from './routes/AppRoute';
 
 const MoviesList = lazy(() => import('./components/MoviesList/MoviesList'));
 const MovieDetails = lazy(
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
       <Suspense fallback={<CircularProgress />}>
         <Routes>
-          <Route path={AppRoute.Home} element={<MoviesList />} />
+          <Route path={AppRoute.MoviesList} element={<MoviesList />} />
           <Route path={AppRoute.MovieDetails} element={<MovieDetails />} />
         </Routes>
       </Suspense>
