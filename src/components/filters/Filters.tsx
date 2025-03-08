8cff49c24b78b44a771279a354850af108ae22a5
ossowskid <77751878+ossowskid@users.noreply.github.com>
2025-03-08 20:18:52 +0100

    feat: created reset filters button

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -6,6 +6,7 @@ import {
   FormControl,
   InputLabel,
   SelectChangeEvent,
+  Button,
 } from '@mui/material';
 import { sortOptions } from './sortOptions';
 import { useGetGenresContext } from '../../context/useGetGenresContext/useGetGenresContext';
@@ -29,6 +30,11 @@ export const Filters = () => {
     },
     [category]
   );
+
+  const handleResetFilters = useCallback(() => {
+    setCategory('');
+    setSortBy('');
+  }, [setCategory, setSortBy]);
 
   if (!genres) {
     return null;
@@ -82,6 +88,14 @@ export const Filters = () => {
           ))}
         </Select>
       </FormControl>
+      <Button
+        variant='outlined'
+        color='primary'
+        onClick={handleResetFilters}
+        sx={{ mt: 2, width: 'fit-content' }}
+      >
+        Resetuj filtry
+      </Button>
     </Box>
   );
 };
