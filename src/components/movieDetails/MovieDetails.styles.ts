a845c0efe9c05e5cc80bf6cb44bd88e2d5a5f0bb
ossowskid <77751878+ossowskid@users.noreply.github.com>
2025-03-09 02:01:46 +0100

    refactor: refactored SkeletonMovieDetails to work properly on smaller resoltions

--- src/components/movieDetails/MovieDetails.styles.ts
+++ src/components/movieDetails/MovieDetails.styles.ts
@@ -2,12 +2,10 @@ import styled from 'styled-components';
 
 export const Image = styled.img`
   @media screen and (max-width: 900px) {
-    min-width: 200px;
-    max-width: 200px;
+    width: 200px;
     height: 300px;
   }
 
-  min-width: 300px;
-  max-width: 300px;
+  width: 300px;
   height: 450px;
 `;
--- src/components/movieDetails/skeletonMovieDetails/SkeletonMovieDetails.tsx
+++ src/components/movieDetails/skeletonMovieDetails/SkeletonMovieDetails.tsx
@@ -1,52 +1,140 @@
-import { Container, Skeleton } from '@mui/material';
 import {
-  SkeletonAdditionalInformationsWrapper,
-  SkeletonBox,
-  SkeletonDetails,
-  SkeletonHeaderWrapper,
-  SkeletonOverviewDetails,
-  SkeletonOverviewWrapper,
-  SkeletonProductionCompanies,
-  SkeletonRatingWrapper,
-} from './SkeletonMovieDetails.styles';
+  Box,
+  Container,
+  Skeleton,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 
 export const SkeletonMovieDetails = () => {
+  const theme = useTheme();
+  const isMaxSmScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const isMinMdScreen = useMediaQuery(theme.breakpoints.up('md'));
+  const isSmToMdScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+
   return (
-    <Container sx={{ display: 'flex', flexFlow: 'column', gap: '20px' }}>
-      <Skeleton variant={'rounded'} width={162} height={40} />
-      <SkeletonBox>
-        <Skeleton variant={'rectangular'} width={300} height={450} />
-        <SkeletonDetails>
-          <SkeletonHeaderWrapper>
-            <Skeleton variant={'rounded'} height={37} width={450} />
-            <Skeleton variant={'rounded'} height={18} width={400} />
-          </SkeletonHeaderWrapper>
-          <SkeletonOverviewWrapper>
+    <Container sx={{ display: 'flex', flexFlow: 'column', gap: '20px', py: 2 }}>
+      <Skeleton variant='rounded' width={162} height={40} />
+      <Box
+        sx={{
+          display: 'flex',
+          flexFlow: { xs: 'column', sm: 'row' },
+          gap: '20px',
+          bgcolor: '#aab9cf',
+          borderRadius: '4px',
+          p: { xs: 2, md: '32px 16px' },
+        }}
+      >
+        {isSmToMdScreen ? (
+          <Box sx={{ display: 'flex', flexFlow: 'column', gap: '20px' }}>
             <Skeleton
-              variant={'rounded'}
-              height={22}
-              width={300}
-              sx={{ marginTop: '19px', marginBottom: '19px' }}
+              variant='rectangular'
+              sx={{
+                width: { xs: 200, sm: 250 },
+                height: { xs: 300, sm: 350 },
+                flexShrink: 0,
+              }}
             />
-            <SkeletonOverviewDetails>
-              <Skeleton variant={'rounded'} height={18} width={40} />
-              <Skeleton variant={'rounded'} height={90} width={650} />
-            </SkeletonOverviewDetails>
-          </SkeletonOverviewWrapper>
-          <SkeletonAdditionalInformationsWrapper>
-            <SkeletonRatingWrapper>
-              <Skeleton variant={'rounded'} height={18} width={147} />
-              <Skeleton variant={'rounded'} height={24} width={240} />
-              <Skeleton variant={'rounded'} height={18} width={122} />
-            </SkeletonRatingWrapper>
-          </SkeletonAdditionalInformationsWrapper>
-        </SkeletonDetails>
-      </SkeletonBox>
-      <SkeletonProductionCompanies>
-        <Skeleton variant={'rectangular'} width={100} height={100} />
-        <Skeleton variant={'rectangular'} width={100} height={100} />
-        <Skeleton variant={'rectangular'} width={100} height={100} />
-      </SkeletonProductionCompanies>
+            <Box sx={{ display: 'flex', flexFlow: 'column', gap: '4px' }}>
+              <Skeleton variant='rounded' height={18} width={147} />
+              <Skeleton variant='rounded' height={24} width={240} />
+              <Skeleton variant='rounded' height={18} width={122} />
+            </Box>
+          </Box>
+        ) : (
+          <Skeleton
+            variant='rectangular'
+            sx={{
+              width: { xs: 200, sm: 250, md: 300 },
+              height: { xs: 300, sm: 350, md: 400 },
+              flexShrink: 0,
+            }}
+          />
+        )}
+        <Box
+          sx={{
+            display: 'flex',
+            flexFlow: 'column',
+            gap: { xs: '20px', sm: 0 },
+            flex: 1,
+            justifyContent: {
+              xs: 'space-between',
+              sm: 'unset',
+              md: 'space-between',
+            },
+            minHeight: { md: 450 },
+          }}
+        >
+          <Box sx={{ display: 'flex', flexFlow: 'column', gap: '8px' }}>
+            <Skeleton
+              variant='rounded'
+              sx={{ width: { xs: '90%', md: 450 }, height: 37 }}
+            />
+            <Skeleton
+              variant='rounded'
+              sx={{ width: { xs: '70%', md: 400 }, height: 18 }}
+            />
+            {!isMinMdScreen && (
+              <>
+                <Skeleton variant='rounded' height={18} width={200} />
+                <Skeleton variant='rounded' height={18} width={100} />
+              </>
+            )}
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexFlow: 'column',
+              gap: '8px',
+              mt: { xs: 2, md: '19px' },
+            }}
+          >
+            <Skeleton
+              variant='rounded'
+              sx={{ width: { xs: '100%', md: 3000 }, height: 22 }}
+            />
+            <Skeleton
+              variant='rounded'
+              sx={{ width: { xs: '100%', md: 650 }, height: 90 }}
+            />
+          </Box>
+          {isMaxSmScreen || isMinMdScreen ? (
+            <Box
+              sx={{
+                display: 'flex',
+                flexFlow: 'column',
+                gap: '4px',
+                mt: 'auto',
+              }}
+            >
+              <Skeleton variant='rounded' height={18} width={147} />
+              <Skeleton variant='rounded' height={24} width={240} />
+              <Skeleton variant='rounded' height={19} width={240} />
+            </Box>
+          ) : null}
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexFlow: 'row wrap',
+          gap: '20px',
+          justifyContent: 'center',
+          alignItems: 'center',
+          bgcolor: '#aab9cf',
+          borderRadius: '4px',
+          p: '32px 16px',
+        }}
+      >
+        {Array.from(new Array(3)).map((_, index) => (
+          <Skeleton
+            key={index}
+            variant='rectangular'
+            width={100}
+            height={100}
+          />
+        ))}
+      </Box>
     </Container>
   );
 };
